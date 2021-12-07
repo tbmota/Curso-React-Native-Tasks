9fbc4868202aa68dd31dfb8f34bc703fb046b869
Thammyrys Mota <thammyrys_@hotmail.com>
2021-12-07 13:53:49 -0300

    first commit

---
+++ src/screens/TaskList.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+
+import todayImage from '../../assets/imgs/today.jpg';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
+
+export default class TaskList extends Component {
+    render() {
+        const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
+        return (
+            <View style={styles.container}>
+                <ImageBackground source={todayImage}
+                    style={styles.background}>
+                        <View style={styles.titleBar}>
+                            <Text>Hoje</Text>
+                            <Text>{today}</Text>
+                        </View>
+
+                </ImageBackground>
+                <View style={styles.taskList}>
+                    <Text>TaskList</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    background: {
+        flex: 3
+    },
+    taskList: {
+        flex: 7
+    },
+    titleBar: {
+        flex: 1,
+        justifyContent: 'flex-end'
+    }
+});
+
+//import 'moment/locale/pt-br'; diz que preciso traduzir para o português
