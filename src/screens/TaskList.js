40dbb291cf774bb40018fecb93d1dbc618fc00e2
Thammyrys Mota <thmota@grupopetropolis.com.br>
2021-12-10 08:10:56 -0300

    versão offline

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -1,28 +1,129 @@
 import React, { Component } from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform, Alert } from 'react-native';
 
-import todayImage from '../../assets/imgs/today.jpg';
+import AsyncStorageStatic from '@react-native-community/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import moment from 'moment';
 import 'moment/locale/pt-br';
 
+import commonStyles from '../commonStyles';
+import todayImage from '../../assets/imgs/today.jpg';
+import Task from '../components/Task'
+import AddTask from './AddTask';
+
+
+const initialState = {
+    showDoneTasks: true,
+    showAddTasks: false,
+    visibleTasks: [],
+    tasks: []
+}
 
 export default class TaskList extends Component {
+    state = {
+        ...initialState
+        // tasks: [{
+        //     id: Math.random(),
+        //     desc: 'Comprar Livro de React Native',
+        //     estimateAt: new Date(),
+        //     doneAt: new Date(),
+        // }, {
+        //     id: Math.random(),
+        //     desc: 'Comprar Livro de React Native',
+        //     estimateAt: new Date(),
+        //     doneAt: null,
+        // }]
+    }
+
+    componentDidMount = async () => {
+        const  stateString = await AsyncStorage.getItem('tasksState')
+        const state = JSON.parse(stateString) || initialState
+        this.setState(state, this.filterTasks)
+    }
+
+    toggleFilter = () => {
+        this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null
+        if(this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+
+        this.setState({ visibleTasks })
+        AsyncStorage.setItem('tasksState', JSON.stringify(this.state))
+    }
+
+    toggleTask = taskId => {
+        const tasks = [...this.state.tasks]
+        tasks.forEach(task => {
+            if(task.id === taskId) {
+                task.doneAt = task.doneAt ? null : new Date()
+            }
+        })
+
+        this.setState({ tasks }, this.filterTasks)
+    }
+
+    addTask = newTask => {
+        if(!newTask.desc || !newTask.desc.trim()) {
+            Alert.alert('Dados Inválidos', 'Descrição não informada!')
+            return
+        }
+        const tasks = [...this.state.tasks]
+        tasks.push({
+            id: Math.random(),
+            desc: newTask.desc,
+            estimateAt: newTask.date,
+            doneAt: null
+        })
+
+        // this.setState({ tasks, showAddTasks: false }, this.filterTasks)
+        this.setState({ tasks, showAddTask: false }, this.filterTasks)
+    }
+
+    deleteTask = id => {
+        const tasks = this.state.tasks.filter(task => task.id !== id)
+        this.setState({ tasks }, this.filterTasks)
+    }
+
     render() {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTasks} 
+                    onCancel={() => this.setState({ showAddTasks: false })} 
+                    onSave={this.addTask} />
                 <ImageBackground source={todayImage}
                     style={styles.background}>
+                        <View style={styles.iconBar}>
+                            <TouchableOpacity onPress={this.toggleFilter}>
+                                <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} 
+                                    size={20} color={commonStyles.colors.secondary} />
+                            </TouchableOpacity>
+                        </View>
                         <View style={styles.titleBar}>
-                            <Text>Hoje</Text>
-                            <Text>{today}</Text>
+                            <Text style={styles.title}>Hoje</Text>
+                            <Text style={styles.subtitle}>{today}</Text>
                         </View>
 
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <Text>TaskList</Text>
+                   <FlatList data={this.state.visibleTasks}
+                        keyExtractor={item => `${item.id}`}
+                        renderItem={({item}) => <Task {...item} onToggleTask={this.toggleTask} onDelete={this.deleteTask}/>} />
                 </View>
+                <TouchableOpacity style={styles.addButton}
+                    activeOpacity={0.7}
+                    onPress={() => this.setState({ showAddTasks: true })}>
+                    <Icon name="plus" size={20}
+                        color={commonStyles.colors.secondary} />
+                </TouchableOpacity>
             </View>
         )
     }
@@ -41,6 +142,37 @@ const styles = StyleSheet.create({
     titleBar: {
         flex: 1,
         justifyContent: 'flex-end'
+    },
+    title: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 50,
+        marginLeft: 20,
+        marginBottom: 20,
+    },
+    subtitle: {
+        fontFamily: commonStyles.fontFamily,
+        color: commonStyles.colors.secondary,
+        fontSize: 20,
+        marginLeft: 20,
+        marginBottom: 30
+    },
+    iconBar: {
+        flexDirection: 'row',
+        marginHorizontal: 20,
+        justifyContent: 'flex-end',
+        marginTop: Platform.OS === 'ios' ? 40 : 10
+    },
+    addButton: {
+        position: 'absolute',
+        right: 30,
+        bottom: 30,
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: commonStyles.colors.today,
+        justifyContent: 'center',
+        alignItems: 'center'
     }
 });
 
